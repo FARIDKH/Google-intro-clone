3046fc84f81df8f0cbf2ac4fb122be4d1f52d094
Farid Ganbarli <faridganbarli@farids-macbook-air.local>
2018-12-30 12:14:24 +0100

    Initial Commit

---
+++ Google Intro CloneTests/Google_Intro_CloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  Google_Intro_CloneTests.swift
+//  Google Intro CloneTests
+//
+//  Created by Farid Ganbarli on 2018. 12. 30..
+//  Copyright © 2018. Farid Ganbarli. All rights reserved.
+//
+
+import XCTest
+@testable import Google_Intro_Clone
+
+class Google_Intro_CloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
